d9e5c465edea872cc844924af7dd01cc1f1e02a0
nlfnc <mconfalo@my.centennialcollege.ca>
2019-10-09 15:50:15 -0400

    Add initial unity files

---
+++ Assets/Scripts/playercController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercController : MonoBehaviour {
+
+    public float Speed;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+    // FixedUpdate is used when modifying physics values
+	void FixedUpdate ()
+    {
+     float horiz = Input.GetAxis("Horizontal");
+     float vert = Input.GetAxis("Vertical");
+        Debug.Log("Horizontal: " + horiz + "Vertical: "+vert);
+
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+
+        rBody.velocity = new Vector2(horiz*Speed, vert*Speed);
+        
+            
+            
+            }
+}
